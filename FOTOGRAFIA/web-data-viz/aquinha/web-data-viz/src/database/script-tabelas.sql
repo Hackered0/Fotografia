b99f7ed72addf4ac01fbe6ff137af60953a372d3
Hackered0 <eduarda.crispim@sptech.school>
2026-06-15 19:00:01 -0300

    Atualização de tabela sql

--- FOTOGRAFIA/web-data-viz/aquinha/web-data-viz/src/database/script-tabelas.sql
+++ FOTOGRAFIA/web-data-viz/aquinha/web-data-viz/src/database/script-tabelas.sql
@@ -1,62 +1,50 @@
--- Arquivo de apoio, caso você queira criar tabelas como as aqui criadas para a API funcionar.
--- Você precisa executar os comandos no banco de dados para criar as tabelas,
--- ter este arquivo aqui não significa que a tabela em seu BD estará como abaixo!
-
-/*
-comandos para mysql server
-*/
-
-CREATE DATABASE aquatech;
-
-USE aquatech;
-
-CREATE TABLE empresa (
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	razao_social VARCHAR(50),
-	cnpj CHAR(14),
-	codigo_ativacao VARCHAR(50)
-);
+CREATE DATABASE fotografia;
+USE fotografia;
 
 CREATE TABLE usuario (
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	nome VARCHAR(50),
-	email VARCHAR(50),
-	senha VARCHAR(50),
-	fk_empresa INT,
-	FOREIGN KEY (fk_empresa) REFERENCES empresa(id)
+    idUsuario INT PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(100) NOT NULL,
+    email VARCHAR(100) UNIQUE NOT NULL,
+    senha VARCHAR(100) NOT NULL,
+    
+    dataCadastro DATETIME DEFAULT CURRENT_TIMESTAMP,
+    CHECK (email LIKE '%@%')
 );
 
-CREATE TABLE aviso (
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	titulo VARCHAR(100),
-	descricao VARCHAR(150),
-	fk_usuario INT,
-	FOREIGN KEY (fk_usuario) REFERENCES usuario(id)
+CREATE TABLE estilo_fotografia (
+    idEstilo INT PRIMARY KEY AUTO_INCREMENT,
+    nomeEstilo VARCHAR(100)
 );
 
-create table aquario (
-/* em nossa regra de negócio, um aquario tem apenas um sensor */
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	descricao VARCHAR(300),
-	fk_empresa INT,
-	FOREIGN KEY (fk_empresa) REFERENCES empresa(id)
+CREATE TABLE resultado_quiz (
+    idResultado INT PRIMARY KEY AUTO_INCREMENT,
+    dataResultado DATETIME DEFAULT CURRENT_TIMESTAMP,
+    fkUsuario INT UNIQUE,
+    fkEstilo INT,
+
+    FOREIGN KEY (fkUsuario) REFERENCES usuario(idUsuario),
+    FOREIGN KEY (fkEstilo) REFERENCES estilo_fotografia(idEstilo)
 );
 
-/* esta tabela deve estar de acordo com o que está em INSERT de sua API do arduino - dat-acqu-ino */
-
-create table medida (
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	dht11_umidade DECIMAL,
-	dht11_temperatura DECIMAL,
-	luminosidade DECIMAL,
-	lm35_temperatura DECIMAL,
-	chave TINYINT,
-	momento DATETIME,
-	fk_aquario INT,
-	FOREIGN KEY (fk_aquario) REFERENCES aquario(id)
-);
-
-insert into empresa (razao_social, codigo_ativacao) values ('Empresa 1', 'ED145B');
-insert into empresa (razao_social, codigo_ativacao) values ('Empresa 2', 'A1B2C3');
-insert into aquario (descricao, fk_empresa) values ('Aquário de Estrela-do-mar', 1);
-insert into aquario (descricao, fk_empresa) values ('Aquário de Peixe-dourado', 2);
+INSERT INTO estilo_fotografia (nomeEstilo) 
+VALUES      ('Retrato'),
+			('Paisagem'),
+			('Natureza'),
+			('Urbana'),
+			('Documental'),
+			('Fotojornalismo'),
+			('Arquitetura'),
+			('Moda'),
+			('Esportiva'),
+			('Vida Selvagem'),
+			('Macro'),
+			('Astrofotografia'),
+			('Noturna'),
+			('Preto e Branco'),
+			('Artística'),
+			('Conceitual'),
+			('Street Photography'),
+			('Minimalista'),
+			('Viagem'),
+			('Casamento');
+            
